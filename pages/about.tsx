170b0aedcc4efc20def83208d92c24b71a5bea97
GandresCoello18 <goyeselcoca@gmail.com>
2020-12-25 16:42:55 -0500

    update dependency from next js to latest version

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Header } from '../components/molecules/Header'
 
-const About = () => <Header title="About" />
+const About = () => <Header title="Aboutee" />
 
 export default About
