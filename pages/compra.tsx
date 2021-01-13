e4547935e83c58c89be8b07a23895d12627de6ed
GandresCoello18 <goyeselcoca@gmail.com>
2021-01-12 19:01:22 -0500

    desing(grascias por la compra): terminar compra y agradecimiento

--- pages/compra.tsx
+++ pages/compra.tsx
@@ -1,13 +1,32 @@
+/* eslint-disable no-unused-expressions */
 import { Store } from 'redux'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { NextSeo } from 'next-seo'
+import { Modal, ModalBody } from 'reactstrap'
+import Link from 'next/link'
+import { FcFaq, FcLike, FcOk, FcPaid, FcShipped } from 'react-icons/fc'
+import { BiHappyBeaming } from 'react-icons/bi'
 import { AllAction, RootState } from '../reducers'
 import { setTitle } from '../reducers/app'
 import Layout from '../components/layout'
 import StepsShopping from '../components/element/steps-shopping'
+import FinishShopping from '../components/svg/finish-shopping'
 
 const Compra = () => {
   const [itemStep] = useState<number>(0)
+  const [finishShopping, setFinishShopping] = useState<boolean>(false)
+  const [visible, setVisible] = useState<boolean>(false)
+
+  const Styles = {
+    text: {
+      color: '#000',
+      textDecoration: 'none',
+    },
+  }
+
+  useEffect(() => {
+    finishShopping && setVisible(true)
+  }, [finishShopping])
 
   return (
     <>
@@ -34,13 +53,91 @@ const Compra = () => {
 
       <Layout>
         <section className="container font-arvo p-md-5">
-          <div className="row justify-between">
+          <div className="row justify-content-between">
             <div className="col-12 bg-white">
-              <StepsShopping startingStep={itemStep} />
+              {finishShopping ? (
+                <div className="row justify-content-center text-center p-3 p-md-5">
+                  <div className="col-12">
+                    <h3 className="p-3">
+                      Gracias por tu compra.!!{' '}
+                      <BiHappyBeaming size={30} color="#f097ac" />
+                    </h3>
+                  </div>
+                  <div className="col-12 col-md-5 mb-4">
+                    <Link href="/">
+                      <a href="/" style={Styles.text}>
+                        <div className="p-3 border-cici border-round box-shadow-cici">
+                          <FcPaid size={20} /> Mis compras (20)
+                        </div>
+                      </a>
+                    </Link>
+                  </div>
+                  <div className="col-12 col-md-5 mb-4">
+                    <Link href="/">
+                      <a href="/" style={Styles.text}>
+                        <div className="p-3 border-cici border-round box-shadow-cici">
+                          <FcLike size={20} /> Mis favoritos (5)
+                        </div>
+                      </a>
+                    </Link>
+                  </div>
+                  <div className="col-12 col-md-5 mb-4">
+                    <Link href="/">
+                      <a href="/" style={Styles.text}>
+                        <div className="p-3 border-cici border-round box-shadow-cici">
+                          <FcFaq size={20} /> Centro de mensajes
+                        </div>
+                      </a>
+                    </Link>
+                  </div>
+                  <div className="col-12 col-md-5 mb-4">
+                    <Link href="/">
+                      <a href="/" style={Styles.text}>
+                        <div className="p-3 border-cici border-round box-shadow-cici">
+                          <FcShipped size={20} /> Mis productos en envio
+                        </div>
+                      </a>
+                    </Link>
+                  </div>
+                </div>
+              ) : (
+                <StepsShopping
+                  startingStep={itemStep}
+                  setFinishShopping={setFinishShopping}
+                />
+              )}
             </div>
           </div>
         </section>
       </Layout>
+
+      <Modal
+        isOpen={visible}
+        toggle={() => setVisible(!visible)}
+        className="font-arvo"
+      >
+        <ModalBody>
+          <strong className="border-bottom p-2">
+            <FcOk size={30} /> Su compra se realizo con exito
+          </strong>
+          <p className="p-3">
+            Despues de 24 horas puede revisar la seccion de mis pedidos en su
+            perfil, para obtener su guia de rastreo. Para saber mas como son
+            nuestras envios puedes{' '}
+            <Link href="/guia-de-compra/rastreo">
+              <a href="/guia-de-compra/rastreo">ver aqui..!</a>
+            </Link>
+          </p>
+
+          <Link href="/productos">
+            <a href="/productos" className="btn text-cici">
+              Ver mis compras
+            </a>
+          </Link>
+
+          <FinishShopping />
+        </ModalBody>
+      </Modal>
     </>
   )
 }
