231a9fa8303ad9157faa97b8115c87d32bec5aa5
Andres Coello <45317893+GandresCoello18@users.noreply.github.com>
2021-04-28 09:54:08 -0500

    change url api cici (#42)

--- api/index.ts
+++ api/index.ts
@@ -4,7 +4,7 @@ export const DEFAULT_AVATAR = 'avatar-default.svg'
 export const BASE_API_IMAGES_CLOUDINNARY =
   'http://res.cloudinary.com/cici/image/upload/v1616791874'
 // const apiDev = 'http://localhost:9000'
-const apiProd = 'http://159.65.169.195:9000'
+const apiProd = 'https://api.cici.beauty'
 
 export const BASE_API = apiProd
 
